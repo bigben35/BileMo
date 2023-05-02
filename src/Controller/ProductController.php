03392e76c7a2d1cc7265e2c07102533a7d4fed25
Josselin Crenn <josselincrenn@gmail.com>
2023-05-02 15:06:14 +0200

    get all products; status 200 OK with Postman

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/products', name: 'product', methods: ['GET'])]
-    public function getAllProducts(): JsonResponse
+    #[Route('/api/products', name: 'products', methods: ['GET'])]
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+
+        $productList = $productRepository->findAll();
+
+        $jsonProductList = $serializer->serialize($productList, 'json');
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
 }
