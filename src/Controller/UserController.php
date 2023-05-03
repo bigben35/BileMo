1a6c66225d936e8c0cf2d5324892ddd0ba89ee07
Josselin Crenn <josselincrenn@gmail.com>
2023-05-03 12:14:08 +0200

    pb authorization, error 404 but no message 'You have no rights' if id user is not linked to client; if id is good (linked to a client)-> status 200 Ok Postman

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,5 +22,21 @@ class UserController extends AbstractController
 
         $jsonProductList = $serializer->serialize($userList, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
+    #[IsGranted('ROLE_USER', message: "Vous n'avez pas les droits suffisants pour accéder à la liste des utilisateurs")]
+    public function getDetailUser(User $user, SerializerInterface $serializer, UserRepository $userRepository): JsonResponse
+    {
+        $client = $this->getUser(); // Récupère le client connecté
+        $user = $userRepository->findOneBy(['id' => $user->getId(), 'client' => $client]);
+        
+        if ($user){
+            $jsonUser = $serializer->serialize($user, 'json',['groups' => 'getUsers']);
+            return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+
     }
 }
